cb3d6c5ab02690379d0b7eb03a43be6869aff274
kenfox0115 <ken.c.fox@gmail.com>
2017-03-04 15:08:49 -0800

    first commit
    includes twitter access tokens

---
+++ twitter_sandbox.py
@@ -0,0 +1,6 @@
+import twitter
+
+api = twitter.Api(consumer_key=[y4466mn59R0Fk0aOgqKPpXHzK],
+                  consumer_secret=[BFXyDey7YiG08W5k6sMenk1Lqc7KKwRY3VJl3lCBLDKc36kk7y],
+                  access_token_key=[58867345-wukkRMORY7aJvBb795hJmTGxv6rApJFeUibbBDpLp],
+                  access_token_secret=[hNsEQkTSQ3qGHv8gizDaAMCxrR0xo1h0gMFojwbf8fUI0])
