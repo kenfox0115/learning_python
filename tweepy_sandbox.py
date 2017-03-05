6204993a505f115f4eb0910651b1f22b88ff7f13
kenfox0115 <ken.c.fox@gmail.com>
2017-03-04 16:06:48 -0800

    first commit
    includes twitter access tokens and code to display logged in user's public tweets  (commented out)

---
+++ tweepy_sandbox.py
@@ -0,0 +1,41 @@
+import tweepy
+
+auth = tweepy.OAuthHandler('y4466mn59R0Fk0aOgqKPpXHzK', 'BFXyDey7YiG08W5k6sMenk1Lqc7KKwRY3VJl3lCBLDKc36kk7y')
+auth.set_access_token('58867345-wukkRMORY7aJvBb795hJmTGxv6rApJFeUibbBDpLp', 'hNsEQkTSQ3qGHv8gizDaAMCxrR0xo1h0gMFojwbf8fUI0')
+
+api = tweepy.API(auth)
+
+API.user_timeline()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#prints public tweets for current user
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print (tweet.text)
--- twitter_sandbox.py
+++ twitter_sandbox.py
@@ -1,6 +1,13 @@
 import twitter
-
-api = twitter.Api(consumer_key=[y4466mn59R0Fk0aOgqKPpXHzK],
-                  consumer_secret=[BFXyDey7YiG08W5k6sMenk1Lqc7KKwRY3VJl3lCBLDKc36kk7y],
-                  access_token_key=[58867345-wukkRMORY7aJvBb795hJmTGxv6rApJFeUibbBDpLp],
-                  access_token_secret=[hNsEQkTSQ3qGHv8gizDaAMCxrR0xo1h0gMFojwbf8fUI0])
+api = twitter.Api(consumer_key='y4466mn59R0Fk0aOgqKPpXHzK',
+                  consumer_secret='BFXyDey7YiG08W5k6sMenk1Lqc7KKwRY3VJl3lCBLDKc36kk7y',
+                  access_token_key='58867345-wukkRMORY7aJvBb795hJmTGxv6rApJFeUibbBDpLp',
+                  access_token_secret='hNsEQkTSQ3qGHv8gizDaAMCxrR0xo1h0gMFojwbf8fUI0')
+print
+api.VerifyCredentials()
+{"id":
+16133,
+"location":
+"Philadelphia",
+"name":
+"bear"}
