9437315e6fedad38c76c790d664202cf0906b4f7
kenfox0115 <ken.c.fox@gmail.com>
2017-03-05 09:54:13 -0800

    first commit. just auth stuff.

---
+++ twitter_monitor.py
@@ -0,0 +1,10 @@
+import tweepy
+
+auth = tweepy.OAuthHandler('y4466mn59R0Fk0aOgqKPpXHzK', 'BFXyDey7YiG08W5k6sMenk1Lqc7KKwRY3VJl3lCBLDKc36kk7y')
+auth.set_access_token('58867345-wukkRMORY7aJvBb795hJmTGxv6rApJFeUibbBDpLp', 'hNsEQkTSQ3qGHv8gizDaAMCxrR0xo1h0gMFojwbf8fUI0')
+
+api = tweepy.API(auth)
+
+
+
+
