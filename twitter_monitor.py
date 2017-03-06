a0955d2868852bcf8eb6dd5e51b06c1e366f54d3
kenfox0115 <ken.c.fox@gmail.com>
2017-03-05 21:14:05 -0800

    changed initial last tweet to pull most recent tweet. seems to work, but needs testing after a new tweet has been made.

--- twitter_monitor.py
+++ twitter_monitor.py
@@ -7,20 +7,15 @@ auth.set_access_token('58867345-wukkRMORY7aJvBb795hJmTGxv6rApJFeUibbBDpLp', 'hNs
 
 api = tweepy.API(auth)
 
-#gets lasttweet id from text file
-lasttweet_fromfile = open('lasttweet.txt', 'r')
-lasttweet = lasttweet_fromfile.read()
-lasttweet_fromfile.close()
+# initially, the script will assume that the last tweet was a null value
+lasttweet = api.user_timeline('KenCFox')[0]
 
-# What the bot will tweet. file reading disabled
+# What the bot will tweet
 #filename = open('twain.txt', 'r')
-tweettext = 'this is dumb'
+tweettext = 'I are the tweet text'
 #filename.close()
 
 
-# a function that picks a random line. disabled
-#def linenum():
-#    return randint(0, len(tweettext))
 
 # this is the function that does most of the work of the bot
 def runTime():
@@ -35,26 +30,15 @@ def runTime():
     # compares the two tweets, and tweets a line of Twain
     # if there is a new tweet from @ocertat
     if mostrecenttweet != lasttweet:
-#        line = tweettext[linenum()]
-        line = '@KenCFox' + tweettext
-        api.update_status(status=line)
+        line = tweettext
+        #api.update_status(status=line)
         print(line)
 
     # updates lasttweet to the most recent tweet
     lasttweet = mostrecenttweet
-
-#writes last tweet to text file
-lasttweet_tofile = open('lasttweet.txt', 'w')
-lasttweet_tofile.write(lasttweet)
-lasttweet_tofile.close()
-
-
 
 # runs the main function every 5 seconds
 while True:
     runTime()
     print("sleeping")
     time.sleep(5)  # Sleep for 5 seconds
-
-
-# To quit early: CTRL+C and wait a few seconds
